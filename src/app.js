5d2ff3c9887c115c33d4a3651111983592e2f305
jmarcossss <joaomalcantaraaa@gmail.com>
2023-01-08 23:19:31 -0300

    instalando as bibliotecas express e cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
-import axios from 'axios';
-// import express from 'express';
-// import cors from 'cors';
+// import axios from 'axios';
+// // import express from 'express';
+// // import cors from 'cors';
 
-console.log("OIOI");
-console.log("TESTANDO");
-console.log(axios);
-// adicionando mais coisas só para testar
+// console.log("OIOI");
+// console.log("TESTANDO");
+// console.log(axios);
+
+import express from 'express';
+
+const app = express(); // Cria um servidor
+
+// Configura uma função pra ser executada quando bater um GET na rota "/"
+app.get("/", (req, res) => {
+    // Manda como resposta o texto 'Hello World'
+    res.send('Hello World againnnininininin');
+});
+
+// Configura o servidor para rodar na porta 4000
+app.listen(4000);
